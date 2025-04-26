6dc4fcce21a15225eda4b324b21adddc93b039a7
jialing1230 <11146013@ntub.edu.tw>
2025-04-26 22:59:26 +0800

    合併 jialing_member 分支，新增會員資料功能 (#3)
    * fix: 顯示錯誤修改完成
    
    * feat: 新增編輯資料表單
    
    * fix: 修正會員編輯資料表格
    
    * fix: 修正保存錯誤
    
    * feat: 增加使用者更換頭像功能
    
    * fix: 修正選單bug
    
    * fix: 修正資料錯誤

--- app/models/preference_sport.py
+++ app/models/preference_sport.py
@@ -1,21 +1,26 @@
-# app/models/preference_sport.py
-
 from sqlalchemy import Column, Integer, ForeignKey, UniqueConstraint
 from sqlalchemy.orm import relationship
 from app.database import Base
+
 
 class PreferenceSport(Base):
     __tablename__ = "preference_sport"
 
     # 使用複合主鍵：preference_id 和 sport_type_id
-    preference_id = Column(Integer, ForeignKey("sport_preferences.preference_id"), primary_key=True)
-    sport_type_id = Column(Integer, ForeignKey("sport_types.sport_type_id"), primary_key=True)
+    preference_id = Column(
+        Integer, ForeignKey("sport_preferences.preference_id"), primary_key=True
+    )
+    sport_type_id = Column(
+        Integer, ForeignKey("sport_types.sport_type_id"), primary_key=True
+    )
 
     # 關聯到 SportPreference 和 SportType
     preference = relationship("SportPreference", back_populates="sports")
     sport_type = relationship("SportType", back_populates="preference_sports")
 
-     # 設置唯一約束
+    # 設置唯一約束
     __table_args__ = (
-        UniqueConstraint('preference_id', 'sport_type_id', name='uq_preference_sport_id'),
+        UniqueConstraint(
+            "preference_id", "sport_type_id", name="uq_preference_sport_id"
+        ),
     )

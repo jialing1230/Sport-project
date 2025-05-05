683f1c141a17449d0170af23cb34fb9f8c87bcf4
yoyo11146030 <11146030@ntub.edu.tw>
2025-05-05 13:46:15 +0800

    加入地圖功能

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from app.app import create_app
-
+from app.routers.google_maps import map_bp
 app = create_app()
+app.register_blueprint(map_bp)
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5002)

8345c10209bf425897798b16b9c9d963c356a4b7
yoyo11146030 <11146030@ntub.edu.tw>
2025-05-06 14:50:43 +0800

    feat: register map blueprint and update route to /map

--- app/routers/map.py
+++ app/routers/map.py
@@ -5,7 +5,7 @@ import os
 
 map_bp = Blueprint("map", __name__)
 
-@map_bp.route("/google_map")
+@map_bp.route("/map")
 def google_map():
     api_key = os.getenv("GOOGLE_MAPS_API_KEY")
     return render_template("map.html", api_key=api_key)

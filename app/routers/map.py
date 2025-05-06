9e43252305d0a81c20f4ae4c95f7a35d756b5021
yoyo11146030 <11146030@ntub.edu.tw>
2025-05-06 14:49:16 +0800

    feat: add Google Maps route and template rendering

---
+++ app/routers/map.py
@@ -0,0 +1,11 @@
+# app/routers/map.py
+
+from flask import Blueprint, render_template
+import os
+
+map_bp = Blueprint("map", __name__)
+
+@map_bp.route("/google_map")
+def google_map():
+    api_key = os.getenv("GOOGLE_MAPS_API_KEY")
+    return render_template("map.html", api_key=api_key)

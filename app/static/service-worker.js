063bf9c845da28c0531f50c8a780f27e57a282dc
jialing1230 <11146013@ntub.edu.tw>
2025-05-06 14:17:42 +0800

    feat: 加入pwa功能

---
+++ app/static/service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "sport-platform-cache-v1";
+const urlsToCache = [
+  "/",
+  "/static/manifest.json",
+  "/static/icons/icon-192x192.png",
+  "/static/icons/icon-512x512.png",
+  // 加入其他需要快取的資源
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+});

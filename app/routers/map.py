db8ef46ef66b20041c8b5dffae08ce1bf8edcfb6
yoyo11146030 <11146030@ntub.edu.tw>
2025-05-06 16:01:49 +0800

    新增地圖功能

--- app/routers/map.py
+++ app/routers/map.py
@@ -9,3 +9,8 @@ map_bp = Blueprint("map", __name__)
 def google_map():
     api_key = os.getenv("GOOGLE_MAPS_API_KEY")
     return render_template("map.html", api_key=api_key)
+@map_bp.route("/home")
+def home_page():
+    api_key = os.getenv("GOOGLE_MAPS_API_KEY")
+    member_id = request.args.get("member_id")  # ⬅️ 接住 URL 上的 member_id
+    return render_template("home.html", api_key=api_key, member_id=member_id)

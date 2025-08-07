7775eb03c9d5dc40e56abea3094ee705b48dbef0
jialing1230 <11146013@ntub.edu.tw>
2025-08-07 17:53:17 +0800

    fix: 刪除加入活動頁面的路由

--- app/app.py
+++ app/app.py
@@ -140,12 +140,6 @@ def create_app():
         member_id = request.args.get("member_id")
         return render_template("introduce.html", member_id=member_id)
     
-    @app.route("/join_activity")
-    def join_activity_page():
-        member_id = request.args.get("member_id")
-        activity_id = request.args.get("activity_id")
-        return render_template("join_activity.html", member_id=member_id, activity_id=activity_id)
-
 
     start_scheduler()  # 啟動排程
     return app

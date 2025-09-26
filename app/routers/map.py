ad5096455307a2dcc73bf3638f2c6d06d1ae0043
yoyo11146030 <11146030@ntub.edu.tw>
2025-09-26 13:19:39 +0800

    多堂課程標點顏色改成藍色

--- app/routers/map.py
+++ app/routers/map.py
@@ -122,7 +122,15 @@ def get_markers_by_county():
 
         # 類型過濾
         if activity_type and activity_type != "all":
-            filters.append(Activity.type == activity_type)
+            if activity_type == "training":
+                # "訓練課程" = class + muti_class
+                from sqlalchemy import or_
+                filters.append(
+                    Activity.type.in_(["class", "muti_class"])
+                )
+            else:
+                # "一般活動" 就是 activity
+                filters.append(Activity.type == activity_type)
 
         activities = (
             db.query(Activity)

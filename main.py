58ffc931b5865a542ce8a067c66288d41a476184
jialing1230 <11146013@ntub.edu.tw>
2025-09-27 19:34:52 +0800

    feat: 虛擬機所需檔案

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from app.routers.map import map_bp
 app = create_app()
 app.register_blueprint(map_bp)
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',debug=True, port=5002)
+    app.run(host='0.0.0.0',debug=True, port=8000)
